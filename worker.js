bdcf583a546f9f974f70d57add2a1a12e11770b0
97250 <97250@DESKTOP-H14PI62>
2021-12-11 01:06:07 +0200

    task

--- worker.js
+++ worker.js
@@ -4,13 +4,15 @@ const namesString =
 const arrayOfNames = namesString.split(",");
 const setNames = new Set(arrayOfNames);
 
-// Find word in string and add it location to the map and return the map results
+// Find word in string and add it location to the map and return the map results, o(n) complexity
 const findWordInString = ({ threadString, numberOfLines }) => {
   const resultedWordsMap = new Map();
   let wordChars = "";
   for (let charIndex = 0; charIndex < threadString.length; charIndex++) {
     wordChars += threadString[charIndex];
     if (setNames.has(wordChars)) {
+      // If chatOffSet need to include all the file text I can count the string lengths in the main thread(like the lines count)
+      // and it to pass findWordInString function and add it to charoffset property
       const resultedObj = {
         lineOffSet: numberOfLines,
         charOffSet: charIndex - wordChars.length,
